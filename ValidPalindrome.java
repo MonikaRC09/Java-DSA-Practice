d824c8f2e4536bee503a6d5ae991c6134314e89d
Monika <mrchowdekar@gmail.com>
2026-06-17 11:37:45 +0530

    Add files via upload
    Java program to check whether a string is a valid palindrome using the Two Pointer approach.

---
+++ ValidPalindrome.java
@@ -0,0 +1,40 @@
+/*
+Problem: Valid Palindrome
+Approach: Two Pointers
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+import java.util.*;
+
+public class ValidPalindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        if(isPalindrome(str)){
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
+    }
+    public static boolean isPalindrome(String str){
+        int left = 0;
+        int right = str.length() - 1;
+        while(left < right){
+            while(left < right && !Character.isLetterOrDigit(str.charAt(left))){
+                left++;
+            }
+            while(left < right && !Character.isLetterOrDigit(str.charAt(right))){
+                right--;
+            }
+            if(Character.toLowerCase(str.charAt(left)) != Character.toLowerCase(str.charAt(right))){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
+
