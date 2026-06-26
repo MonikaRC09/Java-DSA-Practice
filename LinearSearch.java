99f43ca934e76153b67e8132d6a0a34e7d353636
Monika <mrchowdekar@gmail.com>
2026-06-26 19:54:22 +0530

    Updated LinearSearch with complete code implementation

---
+++ LinearSearch.java
@@ -0,0 +1,21 @@
+public class LinearSearch {
+    public static int search(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 8, 2, 9, 1};
+        int target = 2;
+        int result = search(arr, target);
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found.");
+        }
+    }
+}
